a368801a90837ecb432fba139dfb5b9e98246d75
kirti patel <114277505+kirtipatel55@users.noreply.github.com>
2025-01-03 15:51:49 +0530

    Add files via upload

---
+++ travel-with-beauty-main/js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCDv3fhU1q9KBr7NCt6mog5MQcG0ccA1k",
+    authDomain: "travel-and-tourism-d809d.firebaseapp.com",
+    projectId: "travel-and-tourism-d809d",
+    storageBucket: "travel-and-tourism-d809d.appspot.com",
+    messagingSenderId: "1075296524474",
+    appId: "1:1075296524474:web:3116f2b943372824618a83",
+    measurementId: "G-7XW50BX8EQ"
+  };
+
+  firebase.initializeApp(firebaseConfig);
